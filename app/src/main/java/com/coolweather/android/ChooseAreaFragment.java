dd250a895a61d501bffda198000292b42e4ef586
huyouvip <1065566575@qq.com>
2020-04-24 10:51:16 +0000

    add bing backgroud

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -42,9 +42,7 @@ public class ChooseAreaFragment extends Fragment {
         public static final int LEVEL_PROVINCE = 0;
         public static final int LEVEL_CITY = 1;
         public static final int LEVEL_COUNTY = 2;
-        /**
-         * 准备一些控件
-         */
+        //sadasdsad
         private ProgressDialog progressDialog;
         private TextView titleText;
         private Button backButton;
